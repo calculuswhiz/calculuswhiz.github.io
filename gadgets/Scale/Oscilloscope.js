b7576480a07b04f45507b2154216585829358f42
calculuswhiz <calculuswhiz@gmail.com>
2019-02-10 23:03:25 -0500

    Added oscilloscope controls!

--- gadgets/Scale/Oscilloscope.js
+++ gadgets/Scale/Oscilloscope.js
@@ -18,15 +18,33 @@ let Oscilloscope = (function ()
 		{
 			let my = internal(this);
 			
+			function coalesce(props, key, defaultValue)
+			{
+				if (props == null || props[key] === undefined)
+					return defaultValue;
+				else
+					return props[key];
+			}
+			
+			this.beamColor = coalesce(props, 'beamColor', 'green');
+			
 			my.canvas = props.canvas;
 			my.canvas2d = my.canvas.getContext('2d');
 			
 			my.analyzer = props.audioContext.createAnalyser();
-			my.analyzer.fftSize = props.fftSize || 2048;
-			
-			my.dataBuffer = new Uint8Array(my.analyzer.frequencyBinCount);
+			this.setFFTSize(props.fftSize || 2048);
 			
 			this.render();
+		}
+		
+		setFFTSize(newSize)
+		{
+			let my = internal(this);
+			
+			if (newSize >= 0)
+				internal(this).analyzer.fftSize = newSize;
+			
+			my.dataBuffer = new Uint8Array(my.analyzer.frequencyBinCount);
 		}
 		
 		render()
@@ -35,13 +53,14 @@ let Oscilloscope = (function ()
 			
 			let bufferLength = my.analyzer.frequencyBinCount;
 			
+			// Code adapted from MDN website
 			my.analyzer.getByteTimeDomainData(my.dataBuffer);
 			
 			my.canvas2d.fillStyle = '#000000';
 			my.canvas2d.fillRect(0, 0, my.canvas.width, my.canvas.height);
 
 			my.canvas2d.lineWidth = 2;
-			my.canvas2d.strokeStyle = '#00ff00';
+			my.canvas2d.strokeStyle = this.beamColor;
 
 			my.canvas2d.beginPath();
 
@@ -55,11 +74,11 @@ let Oscilloscope = (function ()
 
 				if (i === 0) 
 				{
-				  my.canvas2d.moveTo(x, y);
+				  	my.canvas2d.moveTo(x, y);
 				} 
 				else 
 				{
-				  my.canvas2d.lineTo(x, y);
+				  	my.canvas2d.lineTo(x, y);
 				}
 
 				x += sliceWidth;
