00e6d54d736bf8de17bb9ecf8db097346f46f5de
Some Guy <calculuswhiz@gmail.com>
2022-04-08 13:52:35 -0400

    Added String Tension Calculator

---
+++ gadgets/StringTensionCalculator/app.ts
@@ -0,0 +1,123 @@
+import * as Tensions from './tensions.js';
+
+var counter = 0;
+
+function addString(stat: StringStat = null) {
+    const $listArea = $('#tension-boxes');
+    counter++;
+    const newString = makeStringInput('string-input' + counter, stat);
+    $listArea.append(newString);
+    updateTension(newString);
+}
+
+interface StringStat {
+    pitch: string;
+    material: Tensions.StringMaterial;
+    gauge: number;
+    scaleLength: number;
+};
+
+const presets = {
+    mandolin: [
+        { pitch: 'E5', gauge: 11, material: 'Steel', scaleLength: 14 },
+        { pitch: 'E5', gauge: 11, material: 'Steel', scaleLength: 14 },
+        { pitch: 'A4', gauge: 16, material: 'Steel', scaleLength: 14 },
+        { pitch: 'A4', gauge: 16, material: 'Steel', scaleLength: 14 },
+        { pitch: 'D4', gauge: 22, material: 'PhosphorBronze', scaleLength: 14 },
+        { pitch: 'D4', gauge: 22, material: 'PhosphorBronze', scaleLength: 14 },
+        { pitch: 'G3', gauge: 35, material: 'PhosphorBronze', scaleLength: 14 },
+        { pitch: 'G3', gauge: 35, material: 'PhosphorBronze', scaleLength: 14 },
+    ] as StringStat[]
+};
+
+function loadPreset(preset: string) {
+    $('#tension-boxes').empty();
+    const stats = presets[preset] as StringStat[];
+    for (const stat of stats) {
+        addString(stat);
+    }
+    updateTotal();
+}
+
+function makeStringInput(id: string, stat: StringStat) {
+    const materials = ['Steel', 'FlatwoundSteel', 'PhosphorBronze'];
+    const $el = $('<div>')
+        .prop('id', id)
+        .addClass('string-fields')
+        .append(
+            $('<div>').addClass('input-fields').append(
+                $('<div>').addClass('pitch-field')
+                    .append(
+                        $('<label>').text('Pitch'),
+                        $('<input>').prop({ type: 'text' }).val(stat?.pitch ?? '')
+                    ),
+                $('<div>').addClass('material-field').append(
+                    $('<label>').text('Material'),
+                    $('<select>').append(
+                        materials.map(mtl =>
+                            $('<option>')
+                                .prop({ value: mtl, selected: mtl === stat?.material })
+                                .text(mtl)
+                        )
+                    )
+                ),
+                $('<div>').addClass('gauge-field')
+                    .append(
+                        $('<label>').text('Gauge'),
+                        $('<input>').prop({ type: 'text' }).val(stat?.gauge ?? '')
+                    ),
+                $('<div>').addClass('length-field')
+                    .append(
+                        $('<label>').text('Scale Length'),
+                        $('<input>').prop({
+                            type: 'text',
+                            value: stat?.scaleLength ?? $('input#default-scale').val()
+                        })
+                    )
+            ),
+            $('<div>').addClass('tension-output-field').text('Tension')
+                .append($('<input>').prop({ readonly: true })),
+            $('<input>').prop({type: 'button', value: 'Remove'}).click(_ => $el.remove())
+    );
+
+    $el.find('.input-fields').find('input,select').change(_ => {
+        updateTension($el);
+        updateTotal();
+    });
+
+    return $el;
+}
+
+function updateTension($el: JQuery) {
+    const id = $el.prop('id');
+    const pitch = $(`#${id} .pitch-field input`).val() as string;
+    const material = $(`#${id} .material-field select`).val() as Tensions.StringMaterial;
+    const gauge = $(`#${id} .gauge-field input`).val() as number;
+    const length = $(`#${id} .length-field input`).val() as number;
+
+    console.log(pitch, material, gauge, length);
+
+    const tension = Tensions.calcTension(pitch, material, gauge, length);
+    const pressure = Tensions.calcPressure(gauge, tension);
+    console.log(pressure);
+    const shouldWarn = material === 'Steel'
+        && pressure > Tensions.StainlessYieldStrength;
+
+    $el.find('.tension-output-field input').val(tension);
+    if (shouldWarn)
+        $el.addClass('warning');
+    else
+        $el.removeClass('warning');
+}
+
+function updateTotal() {
+    const total = [...$('.tension-output-field input')]
+        .map(el => +$(el).val())
+        .reduce((a, b) => a + b);
+    $('#total-tension').text(total);
+}
+
+$(document).ready(() => {
+    $('#add-string').click(_ => addString());
+    $('#preset-menu').change(evt => loadPreset((evt.target as HTMLSelectElement).value));
+});
