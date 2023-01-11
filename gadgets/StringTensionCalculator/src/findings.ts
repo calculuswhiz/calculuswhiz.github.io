1d05afc3789463d447422361dd24cf2f73d627f8
Some Guy <calculuswhiz@gmail.com>
2023-01-11 17:11:48 -0500

    Convert to markdown, better regression model (0-intercept).

--- gadgets/StringTensionCalculator/src/findings.ts
+++ gadgets/StringTensionCalculator/src/findings.ts
@@ -1,24 +1,40 @@
 import $ from 'jquery';
+import Showdown from 'showdown';
 
 import { MaterialRegressionEntry } from './tensions';
 
 import './styles.scss';
 import materialData from './materialQuadraticParams.json';
 
-$(() => {
-	function valueToTableData(value: MaterialRegressionEntry) {
-		const {a, b, c, rSq, description} = value;
+function valueToTableData(value: MaterialRegressionEntry) {
+	const {a, b, c, rSq, description} = value;
 
-		return [
-			$('<td>').text(description),
-			$('<td>').text(`a = ${a.toExponential(3)}, b = ${b.toExponential(3)}, c = ${c.toExponential(3)}`),
-			$('<td>').text(rSq)
-		];
-	}
+	return [
+		$('<td>').text(description),
+		$('<td>').text(a.toPrecision(4)),
+		$('<td>').text(b.toPrecision(4)),
+		$('<td>').text(rSq)
+	];
+}
 
-	for (const value of Object.values(materialData) as MaterialRegressionEntry[])
+$(async () => {
+	const converter = new Showdown.Converter(
 	{
-		$('#regressions tbody')
+		tables: true,
+		tablesHeaderId: true
+	});
+	converter.setFlavor('github');
+	const markdown: string = await $.get('/src/findings.md');
+	const html = converter.makeHtml(markdown);
+
+	$('#root').html(html);
+
+	const materialEntries = Object.values(materialData) as MaterialRegressionEntry[];
+	materialEntries.sort((a, b) => b.a - a.a);
+
+	for (const value of materialEntries)
+	{
+		$('#pre-table-1 + table tbody')
 			.append(
 				$('<tr>').append(valueToTableData(value))
 			)
