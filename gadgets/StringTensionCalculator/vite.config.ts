2c490200dd41c2095f2b017cd789e6cd2331da44
Some Guy <calculuswhiz@gmail.com>
2024-04-03 17:32:43 -0400

    Corrected base path for distribution

--- gadgets/StringTensionCalculator/vite.config.ts
+++ gadgets/StringTensionCalculator/vite.config.ts
@@ -1,16 +1,23 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        findings: resolve(__dirname, 'findings.html')
+export default defineConfig(({ command }) => {
+  const base = (command === 'serve')
+    ? ''
+    : '/gadgets/StringTensionCalculator/dist/';
+
+  return ({
+    plugins: [react()],
+    base: base,
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          findings: resolve(__dirname, 'findings.html')
+        }
       }
     }
-  }
-})
+  });
+});
