c586980d003c3e6af928898ce239fe6a7cbb3d61
Zachary Bian <calculuswhiz@gmail.com>
2026-07-30 19:21:44 -0400

    Fix (again), added new gadgets folder

--- gadgets/MortgageCalculator/vite.config.ts
+++ gadgets/MortgageCalculator/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/dist/",
+  base: "/gadgets/MortgageCalculator/dist/",
   build: {
     outDir: 'dist',
   }
